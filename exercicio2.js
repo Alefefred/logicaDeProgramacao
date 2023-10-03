fc9d45032dd7c09db4f778cf0b8ac805a8542f78
Alefefred <alefefred@gmail.com>
2023-10-03 12:53:28 -0300

    Exercício 2

---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+var nome = console.log(prompt("Qual o seu nome?"));
+
+var idade = console.log(prompt("Quantos anos você tem?"));
+
+var linguagem = console.log(prompt("Qual linguagem de programação você está aprendendo?"));
+
+
+document.write("Olá ",nome," você tem ",idade," anos, e já está aprendendo ",linguagem,"!");
+
+//criar uma function
